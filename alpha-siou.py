fb5d69f0f47363b6ce6d44259a23e74cebe67306
rabbie <49755302+a875560134@users.noreply.github.com>
2022-07-15 16:03:39 +0800

    Update alpha-siou.py

--- alpha-siou.py
+++ alpha-siou.py
@@ -3,7 +3,8 @@ import math
 import torch
 
 
-def bbox_iouu(box1, box2, xywh=True, GIoU=True, DIoU=False, CIoU=False, EIoU=False, SIoU=False, a=3, eps=1e-7):
+def bbox_iouu(box1, box2, xywh=True, GIoU=False, DIoU=False, CIoU=False,EIoU=False,SIoU=True,a=3, eps=1e-7):
+
     # Get the coordinates of bounding boxes
     if xywh:  # transform from xywh to xyxy
         (x1, y1, w1, h1), (x2, y2, w2, h2) = box1.chunk(4, 1), box2.chunk(4, 1)
@@ -25,26 +26,27 @@ def bbox_iouu(box1, box2, xywh=True, GIoU=True, DIoU=False, CIoU=False, EIoU=Fal
     beta = 2 * a
     iou = inter / union
     if GIoU or DIoU or CIoU or EIoU or SIoU:
-        cw = torch.max(b1_x2, b2_x2) - torch.min(b1_x1, b2_x1)
-        ch = torch.max(b1_y2, b2_y2) - torch.min(b1_y1, b2_y1)
-        if CIoU or DIoU or EIoU or SIoU:
-            c2 = (cw ** 2 + ch ** 2) ** a + eps
+        cw = torch.max(b1_x2, b2_x2) - torch.min(b1_x1, b2_x1)  # convex (smallest enclosing box) width
+        ch = torch.max(b1_y2, b2_y2) - torch.min(b1_y1, b2_y1)  # convex height
+        if CIoU or DIoU or EIoU or SIoU:  # Distance or Complete IoU https://arxiv.org/abs/1911.08287v1
+            c2 = (cw ** 2 + ch ** 2) ** a + eps  # convex diagonal
             rho_x = torch.abs(b2_x1 + b2_x2 - b1_x1 - b1_x2)
             rho_y = torch.abs(b2_y1 + b2_y2 - b1_y1 - b1_y2)
             rho2 = ((rho_x ** 2 + rho_y ** 2) / 4) ** a
             if DIoU:
                 return iou - rho2 / c2  # DIoU
-            elif CIoU:
+            elif CIoU:  # https://github.com/Zzh-tju/DIoU-SSD-pytorch/blob/master/utils/box/box_utils.py#L47
                 v = (4 / math.pi ** 2) * torch.pow(torch.atan(w2 / h2) - torch.atan(w1 / h1), 2)
                 with torch.no_grad():
-                    alpha = v / ((1 + eps) - inter / union + v)
-                return iou - (rho2 / c2 + torch.pow(v * alpha + eps, a))  # CIoU
+                    alpha_ciou = v / ((1 + eps) - inter / union + v)
+                # return iou - (rho2 / c2 + v * alpha_ciou)  # CIoU
+                return iou - (rho2 / c2 + torch.pow(v * alpha_ciou + eps, a))  # CIoU
             elif EIoU:
-                w_dis = torch.pow(b1_x2 - b1_x1 - b2_x2 + b2_x1, beta)
-                h_dis = torch.pow(b1_y2 - b1_y1 - b2_y2 + b2_y1, beta)
-                cw2 = torch.pow(cw, beta) + eps
-                ch2 = torch.pow(ch, beta) + eps
-                return iou - (rho2 / c2 + w_dis / cw2 + h_dis / ch2)
+                w_dis=torch.pow(b1_x2-b1_x1-b2_x2+b2_x1, beta)
+                h_dis=torch.pow(b1_y2-b1_y1-b2_y2+b2_y1, beta)
+                cw2=torch.pow(cw , beta)+eps
+                ch2=torch.pow(ch , beta)+eps
+                return iou-(rho2/c2+w_dis/cw2+h_dis/ch2)
             else:
                 s_cw = (b2_x1 + b2_x2 - b1_x1 - b1_x2) * 0.5
                 s_ch = (b2_y1 + b2_y2 - b1_y1 - b1_y2) * 0.5
@@ -54,16 +56,16 @@ def bbox_iouu(box1, box2, xywh=True, GIoU=True, DIoU=False, CIoU=False, EIoU=Fal
                 threshold = pow(2, 0.5) / 2
                 sin_alpha = torch.where(sin_alpha_1 > threshold, sin_alpha_2, sin_alpha_1)
                 angle_cost = torch.cos(torch.arcsin(sin_alpha) * 2 - math.pi / 2)
-                rho_x = (s_cw / cw) ** a
-                rho_y = (s_ch / ch) ** a
+                rho_x = (s_cw / cw) ** 2
+                rho_y = (s_ch / ch) ** 2
                 gamma = angle_cost - 2
                 distance_cost = 2 - torch.exp(gamma * rho_x) - torch.exp(gamma * rho_y)
                 omiga_w = torch.abs(w1 - w2) / torch.max(w1, w2)
                 omiga_h = torch.abs(h1 - h2) / torch.max(h1, h2)
-                shape_cost = torch.pow(1 - torch.exp(-1 * omiga_w), beta) + torch.pow(1 - torch.exp(-1 * omiga_h), beta)
-                return iou - 0.5 * (distance_cost + shape_cost)
-        else:
-            c_area = torch.max(cw * ch + eps, union)  # convex area
+                shape_cost = torch.pow(1 - torch.exp(-1 * omiga_w), 4) + torch.pow(1 - torch.exp(-1 * omiga_h), 4)
+                return iou - 0.5 * (torch.pow(distance_cost,a) + torch.pow(shape_cost,a))
+        else:  # GIoU https://arxiv.org/pdf/1902.09630.pdf
+            c_area = torch.max(cw * ch + eps, union) # convex area
             return iou - torch.pow((c_area - union) / c_area + eps, a)  # GIoU
     else:
         return iou  # IoU
